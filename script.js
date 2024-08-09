06d11c858d0ea535bdeeacacae0cc4fd1e3a7571
MariaLopezJim <marialopezj04@gmail.com>
2024-08-08 19:08:56 -0600

    4to commit
    Aquí se le agrego js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //alert("Hola mundo");
 
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('Hola soy María Fernanda López')
+  .pauseFor(300)
+  .deleteChars(20)
+  .typeString('Desarrolladora <strong>Front-End</strong> Jr')
+  .typeString(', dispuesta a crear tu página deseada.')
+  .pauseFor(1000)
+  .start();
